008d453c235d16af912e591d28599bee93e42544
VanThao <VanThao@10.10.11.168>
2018-09-11 18:06:04 -0400

    Van Thao create AddMemberScreen

---
+++ src/group3/lms/ui/AddMemberScreenController.java
@@ -0,0 +1,99 @@
+package group3.lms.ui;
+
+import java.util.Optional;
+
+import javax.swing.JOptionPane;
+
+import group3.lms.common.Common;
+import group3.lms.common.Messages;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+
+public class AddMemberScreenController {
+	@FXML
+	private Button btnNew;
+	@FXML
+	private Button btnSave;
+	@FXML
+	private TextField txtMemberID;
+	@FXML
+	private TextField txtFName;
+	@FXML
+	private TextField txtLName;
+	@FXML
+	private TextField txtStreet;
+	@FXML
+	private TextField txtCity;
+	@FXML
+	private TextField txtState;
+	@FXML
+	private TextField txtZip;
+	@FXML
+	private TextField txtPhone;
+
+	public void clickSave(ActionEvent event) {
+		if (txtMemberID.getText() == null || txtMemberID.getText().equals(""))
+		{
+			Common.ShowMessage(AlertType.INFORMATION, "Member ID is not allowed empty!");
+			txtMemberID.requestFocus();
+		}
+		else if (txtFName.getText() == null || txtFName.getText().equals(""))
+		{
+			Common.ShowMessage(AlertType.INFORMATION, "First Name is not allowed empty!");
+			txtFName.requestFocus();
+		}
+		else if (txtLName.getText() == null || txtLName.getText().equals(""))
+		{
+			Common.ShowMessage(AlertType.INFORMATION, "Last Name is not allowed empty!");
+			txtLName.requestFocus();
+		}
+		else if (txtStreet.getText() == null || txtStreet.getText().equals(""))
+		{
+			Common.ShowMessage(AlertType.INFORMATION, "Street is not allowed empty!");
+			txtStreet.requestFocus();
+		}
+		else if (txtCity.getText() == null || txtCity.getText().equals(""))
+		{
+			Common.ShowMessage(AlertType.INFORMATION, "City is not allowed empty!");
+			txtCity.requestFocus();
+		}
+		else if (txtState.getText() == null || txtState.getText().equals(""))
+		{
+			Common.ShowMessage(AlertType.INFORMATION, "State is not allowed empty!");
+			txtState.requestFocus();
+		}
+		else if (txtZip.getText() == null || txtZip.getText().equals(""))
+		{
+			Common.ShowMessage(AlertType.INFORMATION, "Zip is not allowed empty!");
+			txtZip.requestFocus();
+		}
+		else if (txtPhone.getText() == null || txtPhone.getText().equals(""))
+		{
+			Common.ShowMessage(AlertType.INFORMATION, "Phone is not allowed empty!");
+			txtPhone.requestFocus();
+		}
+		else
+		{}
+	}
+
+	public void clickNew(ActionEvent event) {
+		Optional<ButtonType> result =
+		Common.ShowMessage(AlertType.CONFIRMATION, Messages.INPUT_CREATENEWMEMBER.getValue());
+		if (result.get() == ButtonType.OK) {
+			txtMemberID.clear();
+			txtFName.clear();
+			txtLName.clear();
+			txtStreet.clear();
+			txtCity.clear();
+			txtState.clear();
+			txtZip.clear();
+			txtPhone.clear();
+			txtMemberID.requestFocus();
+		} 
+
+	}
+}
