b8b15a45db2aaea626f86cde190344496ebffa41
VanThao <VanThao@10.10.11.168>
2018-09-11 18:23:36 -0400

    Van Thao commits Message class

--- src/group3/lms/common/Messages.java
+++ src/group3/lms/common/Messages.java
@@ -3,6 +3,7 @@ package group3.lms.common;
 public enum Messages {
 	INPUT_USER_NAME("Please input user name!"),
 	INPUT_PASSWORD("Please input password!"),
+	INPUT_CREATENEWMEMBER("Do you want to create a new member?"),
 	INCORRECT_USER_PASS("User name or Password is incorrect."),;
 	
 	private String value;
