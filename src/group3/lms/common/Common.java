2d717d62d99a4885392c0be93f2594753e378911
109127 <109127@CS_Lab115-204.cs.mum.edu>
2018-09-11 16:36:49 -0500

    login ok

---
+++ src/group3/lms/common/Common.java
@@ -0,0 +1,20 @@
+package group3.lms.common;
+
+import java.util.Optional;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+
+public class Common {
+	public static final String SYSTEM_NAME = "Library Management System";
+
+	public static Optional<ButtonType> ShowMessage(AlertType type, String messge) {
+		Alert alert = new Alert(type);
+		alert.setTitle(SYSTEM_NAME);
+		alert.setHeaderText(null);
+		alert.setContentText(messge);
+
+		return alert.showAndWait();
+	}
+}
--- src/group3/lms/ui/LoginScreenController.java
+++ src/group3/lms/ui/LoginScreenController.java
@@ -1,15 +1,15 @@
 package group3.lms.ui;
 
-import javax.swing.JOptionPane;
+import java.util.Optional;
 
 import group3.lms.business.UserDao;
 import group3.lms.business.entity.User;
+import group3.lms.common.Common;
 import group3.lms.common.Messages;
 import group3.lms.dataaccess.DataAccess;
 import group3.lms.dataaccess.impl.DataAccessFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -26,21 +26,31 @@ public class LoginScreenController {
 	private PasswordField txtPass;
 	@FXML
 	private Button btnLogin;
-
+	
 	public void txtNameEnter(ActionEvent event) {
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+//			Common.ShowMessage(AlertType.ERROR, Messages.INPUT_USER_NAME.getValue());
+			Optional<ButtonType> result =
+					Common.ShowMessage(AlertType.CONFIRMATION, Messages.INPUT_USER_NAME.getValue());
+			if (result.get() == ButtonType.OK) {
+				Common.ShowMessage(AlertType.INFORMATION, "OK");
+			} else {
+				Common.ShowMessage(AlertType.INFORMATION, "NOT OK");
+			}
+			txtName.requestFocus();
+			return;
+		}
 		txtPass.requestFocus();
-		return;
 	}
 
 	public void btnLoginClickMe(ActionEvent event) {
 		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
-//		JOptionPane.showMessageDialog((Stage)ap.getScene().getWindow(), Messages.INPUT_USER_NAME.getValue());
-			new Alert(AlertType.ERROR, Messages.INPUT_USER_NAME.getValue(), ButtonType.OK).show();
+			Common.ShowMessage(AlertType.ERROR, Messages.INPUT_USER_NAME.getValue());
 			txtName.requestFocus();
 			return;
 		}
 		if (txtPass.getText() == null || txtPass.getText().trim().equals("")) {
-			JOptionPane.showMessageDialog(null, Messages.INPUT_PASSWORD.getValue());
+			Common.ShowMessage(AlertType.ERROR, Messages.INPUT_PASSWORD.getValue());
 			txtPass.requestFocus();
 			return;
 		}
@@ -51,7 +61,7 @@ public class LoginScreenController {
 		da.read(dao);
 		User u = dao.getUser(txtName.getText());
 		if (u == null || !txtPass.getText().equals(u.getPassword())) {
-			JOptionPane.showMessageDialog(null, Messages.INCORRECT_USER_PASS.getValue());
+			Common.ShowMessage(AlertType.ERROR, Messages.INCORRECT_USER_PASS.getValue());
 			txtName.requestFocus();
 		}
 	}
